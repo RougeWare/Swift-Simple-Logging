e8a7944074928e435a72b84f029e9c15e26e3700
Ben Leggiero <BenLeggiero@Gmail.com>
2020-08-06 09:44:10 -0600

    #10: Added dynamic library whose name is a valid bundle identifier

--- Package.swift
+++ Package.swift
@@ -19,8 +19,14 @@ let package = Package(
             name: "SimpleLogging",
             targets: ["SimpleLogging"]),
         
+        // DEPRECATED: Does not play nice with App Store requirements
         .library(
             name: "SimpleLogging_dynamic",
+            type: .dynamic,
+            targets: ["SimpleLogging"]),
+        
+        .library(
+            name: "SimpleLoggingDynamic",
             type: .dynamic,
             targets: ["SimpleLogging"]),
     ],
